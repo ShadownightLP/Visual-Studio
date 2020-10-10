6ed6748f13e1abef198dece30e6ba7f1fbdeb2d6
ShadownightLP <58106530+ShadownightLP@users.noreply.github.com>
2020-10-10 03:29:17 +0200

    A new version

---
+++ App/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NetCore.Docker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            var x = "Systemline Written";
+            Console.WriteLine(x);
+            string s  = Environment.CurrentDirectory;  
+            x = "Current Directory is: " + s;
+
+            Console.WriteLine(x);
+
+        }
+    }
+}
