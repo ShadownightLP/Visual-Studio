01e4d0f50142977422759797c96361f8111f0718
ShadownightLP <58106530+ShadownightLP@users.noreply.github.com>
2020-10-14 22:37:19 +0200

    Chaneges made to get an deployment file like i wih to look like

--- App/Program.cs
+++ App/Program.cs
@@ -9,10 +9,11 @@ namespace NetCore.Docker
             Console.WriteLine("Hello World!");
             var x = "Systemline Written";
             Console.WriteLine(x);
-            string s  = Environment.CurrentDirectory;
+            string s  = Environment.CurrentDirectory;  
             x = "Current Directory is: " + s;
             Console.WriteLine(x);
-
+            string stemp2 = Environment.MachineName;
+            Console.WriteLine("The name of this machine is :"+stemp2);
         }
     }
 }
